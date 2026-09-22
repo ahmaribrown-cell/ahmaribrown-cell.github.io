971a0ae6a1eeeaee385f1a14d882fc3a962364c7
ahmaribrown-cell <ahmari.brown@sarahtreed.org>
2026-09-22 20:13:16 +0000

    game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -31,28 +31,31 @@ $(function () {
 
     // TODO 2 - Create Platforms
 createPlatform(500, 630, 50, 290, "red");
-createPlatform(1000, 630, 50, 290, "pink"); 
+createPlatform(700, 630, 50, 290, "pink"); 
+createPlatform(900, 630, 50, 290, "black");
+createPlatform(1100, 630, 50, 290, "yellow");
+createPlatform(1300, 630, 50, 290, "lime"); // bright green for a finished platform
 
-createPlatform(700, 630, 50, 290, "black");
+
 
 
 
     // TODO 3 - Create Collectables
-createCollectable("kennedi", 1350, 50);
-createCollectable("diamond", 200, 170, 0.5, 0.7);
+createCollectable("kennedi", 1200, 500);
+createCollectable("diamond", 400, 170, 0.5, 0.7);
+createCollectable("steve", 350, 50, 1, 0.5);
 
-createCollectable("steve", 1350, 50);
-createCollectable("max", 200, 170, 0.5, 0.7);
 
 
 
 
     
     // TODO 4 - Create Cannons
-createCannon("top", 200, 100);
-createCannon("right", 300, 200);
-createCannon("side", 20, 1000);
-createCannon("bottom", 400, 2000);
+createCannon("top", 300, 700);
+createCannon("top", 650, 700);
+createCannon("right", 450, 2500);
+createCannon("bottom", 450, 2000);
+
 
 
     
